fd2d0929924498ef4590431e74786fbeca10e2aa
mtsxh <hyhfvghg826@gmail.com>
2023-03-05 19:00:30 +0000

    fix certbot renew issue

--- cron/renew_certs.sh
+++ cron/renew_certs.sh
@@ -4,4 +4,5 @@ cd /workdir
 echo "Renewing Let's Encrypt Certificates... (`date`)"
 docker compose  --env-file /docker_env run --rm --no-TTY --entrypoint certbot certbot renew --no-random-sleep-on-renew
 echo "Reloading Nginx configuration"
-docker compose  --env-file /docker_env exec --no-TTY nginx nginx -s reload
+# docker compose  --env-file /docker_env exec --no-TTY nginx nginx -s reload
+docker exec -it nginx nginx -s reload
